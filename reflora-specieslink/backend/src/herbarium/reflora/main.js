7b4a844e0d9661d45a42e27643fb34e469a44119
Alexandre Kajihara <alexandre.ykz@gmail.com>
2019-01-12 00:11:59 -0200

    Várias módulos de requisição

--- reflora-specieslink/backend/src/herbarium/reflora/main.js
+++ reflora-specieslink/backend/src/herbarium/reflora/main.js
@@ -1,3 +1,8 @@
+// import https from 'https';
+// import got from 'got';
+// import superagent from 'superagent';
+// import request from 'request';
+// import axios from 'axios';
 import tombos from '../tombos';
 import database from '../database';
 import queue from '../queue';
@@ -18,6 +23,7 @@ function main() {
         /**
          * Um tombo pode conter dois códigos de barra, porém ambos tem mesma informação,
          * apenas mudando o código de barra. Então eliminamos número de tombo iguais.
+         * Após a execução dessa função teremos 296, dos 300
          */
         /* LOG console.log(dateTime.formatLog('Removendo número de tombos repetidos.')); */
         queue.removeTomboRepetido(queueTombos);
@@ -30,6 +36,7 @@ function main() {
 
                 /**
                  * Chamamos essa função para verificar se esse tombo tem pendência ou não
+                 * Após a execução dessa função teremos 291, dos 300
                  */
                 if (!tombos.temPendencia(tombosAlterados)) {
                     /**
@@ -41,6 +48,58 @@ function main() {
                         /**
                          * Então primeiramente eu vou comparar com as alterações que foram feitas
                          */
+                        // const r = `http://servicos.jbrj.gov.br/v2/herbarium/${tombo.num_barra}`;
+                        /* HTTPS não deu certo sem sinal de vida 
+                        https.get(r, resp => {
+                            let body = '';
+                            resp.on('data', d => {
+                                body += d;
+                            });
+                            resp.on('end', () => {
+                                const parsed = JSON.parse(body);
+                                console.log(parsed);
+                            });
+                        }).on('error', err => {
+                            console.log(`Error: ${err.message}`);
+                        }); */
+                        /* Faz a requisição porém intercala sucesso e problemas 
+                        got(r).then(response => {
+                            console.log(`a${response.body}`);
+                        }).catch(error => {
+                            console.log(`c${error.response.body}`);
+                        });
+                        */
+                        /* Faz a requisição porém intercala sucesso e problemas */
+                        /* superagent.get(r)
+                            .end((err, res) => {
+                                if (err) { console.log(`c${err}`); }
+                                console.log(res.body.success);
+                            });
+                        */
+                        /* Faz a requisição porém intercala sucesso e problemas */
+                        /* request(r, { json: true }, (err, res, body) => {
+                            if (err) { console.log(`c${err}`); }
+                            // console.log(body.url);
+                            // console.log(body.explanation);
+                            console.log(body);
+                            //console.log(tombo.num_barra)
+                        }); */
+                        /* Faz a requisição porém intercala sucesso e problemas - Maneira 1 
+                        axios.all([axios.get(r)])
+                            .then(axios.spread(response1 => {
+                                console.log(response1.data.url);
+                            })).catch(err => {
+                                console.log(err);
+                            }); */
+                        /* Maneira 2 */
+                        /* axios.get(r)
+                            .then(response => {
+                                console.log(response.data.url);
+                                console.log(response.data.explanation);
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            }); */
                         /**
                          * Então faço a requisição ao reflora, se der match com algumas das sugestões
                          * já paro e vou para o próximo tombo.
@@ -51,7 +110,8 @@ function main() {
                          */
                     }
                     /**
-                     * Caso contrário não foi alterado nenhum tombo, portanto só comparar com tabela de tombos
+                     * Caso contrário não foi alterado nenhum tombo, portanto só comparar com tabela de tombos.
+                     * Dos 300, 35 não tem pendências.
                      */
 
                 }
