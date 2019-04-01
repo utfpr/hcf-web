97981d962ad2f269076b935907d1b95d64187e3a
Alexandre Kajihara <alexandre.ykz@gmail.com>
2019-04-01 00:42:08 -0300

    Adicionando mais comentários esquecidos #18

--- reflora-specieslink/herbario-backend/src/controllers/reflora-controller.js
+++ reflora-specieslink/herbario-backend/src/controllers/reflora-controller.js
@@ -2,9 +2,6 @@
 import moment from 'moment';
 import {
     getHoraAtual,
-    // leLOG,
-    // transformaNomeLog,
-    // tempoGastoLog,
 } from '../herbarium/log';
 import {
     criaConexao,
@@ -14,17 +11,22 @@ import {
     selectExecutandoReflora,
 } from '../herbarium/database';
 
+/**
+ * A função preparaRequisicao, faz um select no banco verificando se tem registros
+ * onde o horário de fim é nulo e o serviço é Reflora. Se o resultado dessa consulta
+ * é maior que zero significa que foi retornado algum registro. Se existe algum registro no BD,
+ * onde a data de fim é nula e o serviço é Reflora eu verifico a periodicidade que é. Se a
+ * periodicidade for manual, ele não pode nem agendar nem pedir novamente. Agora se a periodicidade
+ * for semanal, mensal ou a cada dois meses, verificamos se a data atual é diferente dá data de
+ * próxima atualização se for eu atualizo com o novo valor, independentemente se é manual ou periódica.
+ * Caso seja a mesma data não poderá ser feito a troca.
+ * @param {*} request, é a requisição vinda do front end, às vezes pode
+ * conter alguns parâmetros nesse cabeçalhos para conseguir informações
+ * específicas.
+ * @param {*} response, é a resposta que será enviada ao back end.
+ * @param {*} next, é utilizado para chamar a próxima função da pilha.
+ */
 export const preparaRequisicao = (request, response, next) => {
-    /**
-     * Então primeiramente faço um select no banco verificando se tem registros
-     * onde o horário de fim é nulo e o serviço é REFLORA. Se o resultado dessa consulta
-     * é maior que zero significa que foi retornado algum registro. Se existe algum registro no BD,
-     * onde a data de fim é nula e o serviço é Reflora eu verifico a periodicidade que é. Se a
-     * periodicidade for manual, ele não pode nem agendar nem pedir novamente. Agora se a periodicidade
-     * for semanal, mensal ou a cada dois meses, verificamos se a data atual é diferente dá data de
-     * próxima atualização se for eu atualizo com o novo valor, independentemente se é manual ou periódica.
-     * Caso seja a mesma data não poderá ser feito a troca.
-     */
     const { periodicidade } = request.query;
     const proximaAtualizacao = request.query.data_proxima_atualizacao;
     const conexao = criaConexao();
@@ -33,17 +35,14 @@ export const preparaRequisicao = (request, response, next) => {
             const periodicidadeBD = listaExecucaoReflora[0].dataValues.periodicidade;
             if (periodicidadeBD === 'MANUAL') {
                 response.status(200).json(JSON.parse(' { "result": "failed" } '));
-                // conexao.close();
             } else if ((periodicidadeBD === 'SEMANAL') || (periodicidadeBD === '1MES') || (periodicidadeBD === '2MESES')) {
                 if (moment().format('DD/MM/YYYY') !== listaExecucaoReflora[0].dataValues.data_proxima_atualizacao) {
                     const { id } = listaExecucaoReflora[0].dataValues;
                     atualizaInicioTabelaConfiguracao(conexao, id, getHoraAtual(), null, periodicidade, proximaAtualizacao).then(() => {
                         response.status(200).json(JSON.parse(' { "result": "success" } '));
-                        // conexao.close();
                     });
                 } else {
                     response.status(200).json(JSON.parse(' { "result": "failed" } '));
-                    // conexao.close();
                 }
             }
         } else {
@@ -63,6 +62,19 @@ export const preparaRequisicao = (request, response, next) => {
     });
 };
 
+/**
+ * A função estaExecutando, faz um select no banco verificando se tem registros
+ * de execução do Reflora. Se tem execução do reflora é verificado a periodicidade
+ * se a periodicidade é manual, eu envio um JSON com informações de que está executando
+ * e que a periodicidade é manual (MANUAL === execução imediata). O mesmo processo vale para
+ * uma periodicidade que é agendada. Caso não seja retornando nada é retornando ao front
+ * end que não está sendo executado.
+ * @param {*} request, é a requisição vinda do front end, às vezes pode
+ * conter alguns parâmetros nesse cabeçalhos para conseguir informações
+ * específicas.
+ * @param {*} response, é a resposta que será enviada ao back end.
+ * @param {*} next, é utilizado para chamar a próxima função da pilha.
+ */
 export const estaExecutando = (request, response, next) => {
     const conexao = criaConexao();
     selectExecutandoReflora(conexao).then(listaExecucaoReflora => {
--- reflora-specieslink/herbario-backend/src/routes/reflora.js
+++ reflora-specieslink/herbario-backend/src/routes/reflora.js
@@ -1,6 +1,13 @@
 const controller = require('../controllers/reflora-controller');
 const controllerComum = require('../controllers/herbariovirtual-controller');
 
+/**
+ * Essa variável app, está relacionada as rotas que vem do front end. Então se no front end
+ * é feito uma requisição que ao backend que é uma dessas requisições: /reflora,
+ * /reflora-executando, /reflora-todoslogs, /reflora-log, ela irá chamar
+ * a sua respectiva função, que no caso da URL /reflora é preparaRequisição, e assim
+ * por diante.
+ */
 export default app => {
     app.route('/reflora').get([
         controller.preparaRequisicao,
