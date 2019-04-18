4014aee9d07686d193cf9e6fd7fa4c4650b82f14
Alexandre Kajihara <alexandre.ykz@gmail.com>
2019-04-18 14:08:48 -0300

    Ajustando mais detalhes do backend pt2

--- reflora-specieslink/herbario-backend/src/app.js
+++ reflora-specieslink/herbario-backend/src/app.js
@@ -8,6 +8,9 @@ import errors from './middlewares/erros-middleware';
 
 import { storage } from './config/upload';
 
+import { daemonFazRequisicaoReflora } from './herbarium/reflora/main';
+import { daemonSpeciesLink } from './herbarium/specieslink/main';
+
 const app = express();
 app.use(cors());
 app.use(parser.json());
@@ -17,5 +20,13 @@ app.use('/fotos', express.static(storage));
 
 app.use('/api', routes);
 app.use(errors);
+
+/**
+ * Essas duas daemon são utilizadas, ela são iniciadas juntamente
+ * com o back end. Essas daemon de tempos em tempos verificam
+ * se é necessário realizar o processo de atualização do serviço.
+ */
+daemonFazRequisicaoReflora();
+daemonSpeciesLink();
 
 export default app;
---
+++ reflora-specieslink/herbario-backend/src/models/Reflora.js
@@ -0,0 +1,39 @@
+function associate(modelos) {
+}
+
+export default (Sequelize, DataTypes) => {
+
+    const attributes = {
+        cod_barra: {
+            type: DataTypes.STRING(12),
+            allowNull: false,
+            primaryKey: true,
+        },
+        tombo_json: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        ja_comparou: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        ja_requisitou: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+    };
+
+    const options = {
+        freezeTableName: false,
+        // Disabilita o created_at e updated_at
+        timestamps: false,
+        tableName: 'reflora',
+        id: false,
+    };
+
+    const Model = Sequelize.define('reflora', attributes, options);
+
+    Model.associate = associate;
+
+    return Model;
+};
