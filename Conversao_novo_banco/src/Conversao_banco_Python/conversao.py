73a634837a98596781785db425250393792ab6f5
RyanRamos01 <ryan.lramos@hotmail.com>
2024-08-22 16:20:26 -0300

    fix: latitudes de cidades quebradas e descrição do local de coleta

--- Conversao_novo_banco/src/Conversao_banco_Python/conversao.py
+++ Conversao_novo_banco/src/Conversao_banco_Python/conversao.py
@@ -1083,6 +1083,22 @@ def main():
 
     print("Concluído!")
 
+    print("Corrigindo latitudes e longitudes de cidades")
+
+    conexaoSql = conexaoNova.getConexao()
+    cursorNova = conexaoSql.cursor()
+
+    with open('Cidades_Estados_Paises/updated_cities_coordinates.sql', 'r') as sql_file:
+        sql_queries = sql_file.read()
+
+    for query in sql_queries.split(';'):
+        if query.strip():
+            cursorNova.execute(query)
+
+    conexaoSql.commit()
+
+    print("Concluído!")
+
     # -----------------------Insere dados locais_coleta-------------------------------------
     print("Processando Locais Coleta! Aguarde...")
     locais_coletaData = databaseAntiga.getConteudoTabela("local_coleta", "SELECT codigo, local, regiao_do_local, cidade, estado, pais FROM local_coleta")
@@ -1101,7 +1117,7 @@ def main():
     conexaoLocais_coleta = conexaoNova.getConexao()
     for locais_coleta in locais_coletaData:
         cidadeId = buscaCidadeId(cidadeLista, estadoLista, paisLista, locais_coleta)
-        commitLocais_coletaData = (locais_coleta[0], locais_coleta[1] if locais_coleta[1] else "" + locais_coleta[2] if locais_coleta[2] else "", cidadeId, None, None, None)
+        commitLocais_coletaData = (locais_coleta[0], (locais_coleta[1] if locais_coleta[1] else "") + (locais_coleta[2] if locais_coleta[2] else ""), cidadeId, None, None, None)
         databaseNova.insertConteudoTabela("locais_coleta", sql, commitLocais_coletaData, conexaoLocais_coleta )
     print("Concluído!")
 
@@ -1594,7 +1610,6 @@ def main():
 
             commitTombos_fotosData = (tombos_fotos[0], tombos_fotos[2], tombos_fotos[3], caminho_foto, 1, tombos_fotos[1], 1)
             databaseNova.insertConteudoTabela("tombos_fotos", sql, commitTombos_fotosData, conexaoTombos_fotos)
-
     print("Concluído!")
 
     print("Inserindo tipos dos usuários")
@@ -1609,6 +1624,8 @@ def main():
 
     for i, tipo in enumerate(tipos, start = 1):
         databaseNova.insertConteudoTabela("tipos_usuarios", sql, (i, tipo), conexaoTipos_usuarios)
+
+    conexaoTipos_usuarios.close()
         
     print("Concluído")
 
