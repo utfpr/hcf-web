5bde50d56de4b378142bc3818297bed1e5aac213
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2025-04-30 01:47:29 -0300

    fix

--- Conversao_banco_Python/teste.py
+++ Conversao_banco_Python/teste.py
@@ -1,67 +1,13 @@
-import hashlib
+import os
 import fdb
-import mysql.connector
-import psycopg2
 
-def gerar_hash_linha(linha):
-    return hashlib.sha256(
-        '|'.join([str(v) if v is not None else 'NULL' for v in linha]).encode('utf-8')
-    ).hexdigest()
+connection = fdb.connect(
+  host='127.0.0.1', database='/firebird/data/HERBARIUM.GDB',
+  user='SYSDBA', password=os.getenv('ISC_PASSWORD')
+)
 
-def exportar_hashes_firebird():
-    conn = fdb.connect(
-        host='127.0.0.1',
-        database='./../firebird/data/HERBARIUM.GDB',
-        user='SYSDBA',
-        password='masterkey',
-        charset='UTF-8', 
-    )
-    cursor = conn.cursor()
-    tabela = 'tombo'
-    cursor.execute(f'SELECT * FROM {tabela}')
-    hashes = [gerar_hash_linha(row) for row in cursor.fetchall()]
-    with open(f"{tabela}_hashes_firebird.txt", "w") as f:
-        for h in hashes:
-            f.write(h + "\n")
-    print(f"{len(hashes)} linhas exportadas do Firebird")
-    cursor.close()
-    conn.close()
+cursor = connection.cursor()
 
-def exportar_hashes_mysql():
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Test@123',
-        database='hcf'
-    )
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tombos")
-    hashes = [gerar_hash_linha(row) for row in cursor.fetchall()]
-    with open("tombos_hashes_mysql.txt", "w") as f:
-        for h in hashes:
-            f.write(h + "\n")
-    print(f"{len(hashes)} linhas exportadas do MySQL")
-    cursor.close()
-    conn.close()
+cursor.execute('SELECT COUNT(1) FROM TOMBO')
 
-def exportar_hashes_postgres():
-    conn = psycopg2.connect(
-        host='localhost',
-        user='postgres',
-        password='postgres',
-        dbname='hcf'
-    )
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM tombos")
-    hashes = [gerar_hash_linha(row) for row in cursor.fetchall()]
-    with open("tombos_hashes_postgres.txt", "w") as f:
-        for h in hashes:
-            f.write(h + "\n")
-    print(f"{len(hashes)} linhas exportadas do PostgreSQL")
-    cursor.close()
-    conn.close()
-
-if __name__ == "__main__":
-    exportar_hashes_firebird()
-    exportar_hashes_mysql()
-    exportar_hashes_postgres()
+print(cursor.fetchall())
