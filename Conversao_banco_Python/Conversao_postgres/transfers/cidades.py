f5b8b8cdbfcf50156897c4a0bcd01ae12a93da94
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2025-06-18 22:55:34 -0300

    remove a verificação de duplicidade

--- Conversao_banco_Python/Conversao_postgres/transfers/cidades.py
+++ Conversao_banco_Python/Conversao_postgres/transfers/cidades.py
@@ -7,17 +7,14 @@ def transferCities(databaseNova, conexaoNova):
     cidadesData = ''
     with open('../Cidades_Estados_Paises/municipios.csv', newline='', encoding='UTF-8') as csvfile:
         csvReader = csv.reader(csvfile, delimiter=';')
-        next(csvReader)  # Pula o cabeçalho
+        # next(csvReader)  # Pula o cabeçalho
         cidadesData = list(csvReader)
     
     sql = ("INSERT INTO cidades "
            "(id, estado_id, nome, latitude, longitude) "
            "VALUES (%s, %s, %s, %s, %s)")
-    
-    sql_select = "SELECT nome FROM cidades WHERE nome = %s"
-    
+        
     conexaoCidades = conexaoNova.getConexao()
-    cursor = conexaoCidades.cursor()
     
     for cidade in cidadesData:
         city_name = cidade[1]
@@ -26,15 +23,9 @@ def transferCities(databaseNova, conexaoNova):
         state_name = get_state_name_by_id(conexaoCidades, state_id)
         latitude, longitude = get_coordinates_from_city(city_name, state_name)
         
-        cursor.execute(sql_select, (city_name,))
-        resultado = cursor.fetchone()
-        cursor.fetchall()  # Limpa resultados pendentes
-        
-        if resultado is None:
-            commitCidadesData = (cidade[0], cidade[2], city_name, latitude, longitude)
-            databaseNova.insertConteudoTabela("cidades", sql, commitCidadesData, conexaoCidades)
+        commitCidadesData = (cidade[0], cidade[2], city_name, latitude, longitude)
+        databaseNova.insertConteudoTabela("cidades", sql, commitCidadesData, conexaoCidades)
     
-    cursor.close()
     print("Concluído!")
     
     print("Corrigindo latitudes e longitudes de cidades")
