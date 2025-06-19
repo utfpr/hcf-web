6567993794ba3be8831ce310b56af44f29c8097f
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2025-06-18 22:55:41 -0300

    remove a verificação de duplicidade

--- Conversao_banco_Python/Conversao_postgres/transfers/estados.py
+++ Conversao_banco_Python/Conversao_postgres/transfers/estados.py
@@ -12,20 +12,11 @@ def transferStates(databaseNova, conexaoNova):
     sql = ("INSERT INTO estados "
            "(id, nome, sigla, codigo_telefone, pais_id) "
            "VALUES (%s, %s, %s, %s, %s)")
-    
-    sql_select = "SELECT nome FROM estados WHERE nome = %s"
-    
+        
     conexaoEstados = conexaoNova.getConexao()
-    cursor = conexaoEstados.cursor()
     
     for estado in estadosData:
-        cursor.execute(sql_select, (estado[2],))
-        resultado = cursor.fetchone()
-        cursor.fetchall()  # Limpa resultados pendentes
-        
-        if resultado is None:
-            commitEstadosData = (estado[0], estado[2], estado[1], None, estado[3])
-            databaseNova.insertConteudoTabela("estados", sql, commitEstadosData, conexaoEstados)
+        commitEstadosData = (estado[0], estado[2], estado[1], None, estado[3])
+        databaseNova.insertConteudoTabela("estados", sql, commitEstadosData, conexaoEstados)
     
-    cursor.close()
     print("Concluído!")
